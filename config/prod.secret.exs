037a9b940552f4d3305186709745fad8630fa9c6
coronel <carlos.janu@serpro.gov.br>
2018-04-27 16:26:03 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,12 +9,15 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :app, AppWeb.Endpoint,
-  secret_key_base: "jv4uSv8HfE315v1FBIUQJRgUFqNI5Q46V/mU8GM6odYLbqKHAi2kV6oWjDfzrIu9"
+  #  secret_key_base: "jv4uSv8HfE315v1FBIUQJRgUFqNI5Q46V/mU8GM6odYLbqKHAi2kV6oWjDfzrIu9"
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :app, App.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "app_prod",
-  pool_size: 15
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
