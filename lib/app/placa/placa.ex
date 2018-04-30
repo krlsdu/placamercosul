74bb8d4e049029cb870bbb5074ac305aa9304c97
coronel <carlos.janu@serpro.gov.br>
2018-04-30 11:35:36 -0300

    Update reference 0-9 A-J

--- lib/app/placa/placa.ex
+++ lib/app/placa/placa.ex
@@ -106,16 +106,16 @@ defmodule App.Placa do
 
   def placa_mercosul(placa) do
     placa_nova = fn
-      "1" -> "A"
-      "2" -> "B"
-      "3" -> "C"
-      "4" -> "D"
-      "5" -> "E"
-      "6" -> "F"
-      "7" -> "G"
-      "8" -> "H"
-      "9" -> "I"
-      "0" -> "J"
+      "0" -> "A"
+      "1" -> "B"
+      "2" -> "C"
+      "3" -> "D"
+      "4" -> "E"
+      "5" -> "F"
+      "6" -> "G"
+      "7" -> "H"
+      "8" -> "I"
+      "9" -> "J"
     end
 
     placa = Map.fetch!(placa, "placa_mercosul")
